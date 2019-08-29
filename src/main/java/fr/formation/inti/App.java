322dc3d9f5186be05b40125a502d148e0fedccda
IN-LL-025 <IN-LL-025@IN-LL-025>
2019-08-29 14:45:24 +0200

    V2

--- src/main/java/fr/formation/inti/App.java
+++ src/main/java/fr/formation/inti/App.java
@@ -1,20 +1,17 @@
 package fr.formation.inti;
 
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import fr.formation.inti.beans.HelloBean;
 
-/**
- * Hello world!
- *
- */
 public class App
 {
     public static void main( String[] args )
     {
-//    	AbstractApplicationContext context =new ClassPathXmlApplicationContext("applicationContext.xml");
-//    	HelloBean hb = context.getBean("hello",HelloBean.class);
+    	AbstractApplicationContext context =new ClassPathXmlApplicationContext("applicationContext.xml");
+    	HelloBean hb = context.getBean("hello",HelloBean.class);
 //    	hb.hello();
-    	System.out.println(1);
-    	HelloBean a = HelloBean.createInstance();
-//    	context.close();
+    	context.close();
     }
 }
--- src/main/java/fr/formation/inti/beans/HelloBean.java
+++ src/main/java/fr/formation/inti/beans/HelloBean.java
@@ -1,8 +1,26 @@
 package fr.formation.inti.beans;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component("hello")
 public class HelloBean {
+	
+	@Autowired
+	@Qualifier("messageBean")
+	private MessageBean m;
+	
+	public MessageBean getM() {
+		return m;
+	}
+
+	public void setM(MessageBean m) {
+		this.m = m;
+	}
 
 	public HelloBean() {
+		m.getMess();
 	}
 	
 	public void hello() {
